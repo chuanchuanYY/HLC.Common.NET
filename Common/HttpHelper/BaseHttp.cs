7d71edf4eedb72a2011114c9b7beffe78ae0cbfd
chuan <2012430493@qq.com>
2023-10-05 23:22:47 +0800

    Add HttpHelper

---
+++ Common/HttpHelper/BaseHttp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpHelper
+{
+	public class BaseHttp 
+	{
+        public  string? BaseUrl { get; set; }
+		public Double TimeOut { get; set; } = 100000;
+		protected Action<HttpClient>? Configuration { get; set; }
+		protected Action<Exception>? OnError { get; set; }
+		public AuthenticationHeaderValue? Authentication { get; set; }	
+		async Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage)
+		{
+			using var client = new HttpClient();
+			Configuration?.Invoke(client);
+			client.DefaultRequestHeaders.Authorization = Authentication;
+			CancellationTokenSource cancellationToken = new CancellationTokenSource();
+			cancellationToken.CancelAfter(TimeSpan.FromMilliseconds(TimeOut));
+			return await client.SendAsync(requestMessage, cancellationToken.Token);
+		}
+		HttpResponseMessage Send(HttpRequestMessage requestMessage)
+		{
+			using var client = new HttpClient();
+			Configuration?.Invoke(client);
+			client.DefaultRequestHeaders.Authorization = Authentication;
+			CancellationTokenSource cancellationToken = new CancellationTokenSource();
+			cancellationToken.CancelAfter(TimeSpan.FromMilliseconds(TimeOut));
+			return client.Send(requestMessage, cancellationToken.Token);
+		}
+		#region Get
+		protected async Task<HttpResponseMessage>  GetAsync(string url)
+		{
+			using HttpRequestMessage requestMessage = new HttpRequestMessage();
+			requestMessage.Method = HttpMethod.Get;
+			requestMessage.RequestUri = new Uri(BaseUrl+url);
+			return await SendAsync(requestMessage);
+		}
+		protected HttpResponseMessage Get(string url)
+		{
+			using HttpRequestMessage requestMessage = new HttpRequestMessage();
+			requestMessage.Method = HttpMethod.Get;
+			requestMessage.RequestUri = new Uri(BaseUrl + url);
+			return  Send(requestMessage);
+		}
+		#endregion
+		#region Post
+		protected async Task<HttpResponseMessage> PostAsync(string url,object? data=null)
+		{
+			using HttpRequestMessage requestMessage = new HttpRequestMessage();
+			requestMessage.Method = HttpMethod.Post;
+			requestMessage.RequestUri = new Uri(BaseUrl + url);
+			requestMessage.Content = new StringContent(JsonSerializer.Serialize(data==null?"":data)
+				, Encoding.UTF8, "application/json");
+			return await SendAsync(requestMessage);	
+		}
+		protected HttpResponseMessage Post(string url, object? data = null)
+		{
+			using HttpRequestMessage requestMessage = new HttpRequestMessage();
+			requestMessage.Method = HttpMethod.Post;
+			requestMessage.RequestUri = new Uri(BaseUrl + url);
+			requestMessage.Content = new StringContent(JsonSerializer.Serialize(data == null ? "" : data)
+				,Encoding.UTF8,"application/json");//这个后面可以给配置
+			return Send(requestMessage);
+		}
+		#endregion
+		protected void Delete()
+		{
+			
+		}
+		protected void Put()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Common/HttpHelper/LcHttp.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpHelper
+{
+	public class LcHttp:BaseHttp
+	{
+
+		#region  构造函数可选配置
+		public LcHttp()
+        {
+			
+		}
+		public LcHttp(string baseUrl,double timeOut,Action<HttpClient> config)
+		{ 
+			baseUrl = baseUrl ?? string.Empty;	
+			this.TimeOut = timeOut>=0?timeOut:100000;
+		    this.Configuration= config;	
+		}
+
+		#endregion
+		private HttpResponseMessage? _response;
+		private event Action<HttpResponseMessage> onResponsed;
+		public LcHttp then(Action<HttpResponseMessage> response) {
+
+			onResponsed += res => {
+				if (_response!.StatusCode == System.Net.HttpStatusCode.OK)
+					response?.Invoke(_response);
+			};
+			return this;
+		}
+
+		public LcHttp Catch(Action<HttpResponseMessage> error)
+		{
+			onResponsed += res =>
+			{
+				if (_response!.StatusCode != System.Net.HttpStatusCode.OK)
+					error?.Invoke(_response);
+			};
+			return this;
+		}
+		public LcHttp Finlly(Action action)
+		{
+			action?.Invoke();
+			return this;
+		}
+		#region 拦截器
+
+		public void  RequstInterceptor(Action<HttpClient> config,Action<Exception> error=null!)
+		{
+			this.Configuration += config;
+			if(error!=null)
+			this.OnError += error;
+		}
+
+		public void ResponseInterceptor(Action<HttpContent> response,
+			Action<HttpResponseMessage> error =null!)
+		{
+			then(res =>
+			{
+				response.Invoke(res.Content);
+			});
+			Catch((err) => {
+				if (error != null)
+					error.Invoke(err);
+			});
+		}
+		#endregion
+		#region Get 
+
+		public new  LcHttp Get(string url)
+		{
+			try
+			{
+				_response = base.Get(url);
+				onResponsed.Invoke(_response);
+				return this;
+			}
+			catch (Exception ex)
+			{
+
+				this.OnError?.Invoke(ex);
+				return this;
+			}
+			
+		   
+		}
+		public new LcHttp Get(string url,Dictionary<string,object> _params)
+		{
+			 StringBuilder stringBuilder = new StringBuilder();
+			 var keys=_params.Keys;
+			foreach (var key in keys)
+			{
+				stringBuilder.Append($"{key}={_params[key]}&");
+			}
+			stringBuilder.Remove(stringBuilder.Length-1,1);
+			try
+			{
+				_response = base.Get($"{url}?{stringBuilder.ToString()}");
+				onResponsed.Invoke(_response);
+				return this;
+			}
+			catch (Exception ex)
+			{
+				this.OnError?.Invoke(ex);
+				return this;
+			}
+			
+		}
+		#endregion
+
+		#region Post
+		public new LcHttp Post(string url, object? data = null)
+		{
+			try
+			{
+				_response = base.Post(url, data);
+				onResponsed.Invoke(_response);
+				return this;
+			}
+			catch (Exception ex)
+			{
+				this.OnError?.Invoke(ex);
+				return this;
+			}
+		
+		}
+		#endregion
+
+
+	}
+}
